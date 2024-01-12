ba50d30e8b2a1b5c741acb00051f44a49ac02b84
shams-ivon <shams919ivon@gmail.com>
2024-01-12 17:23:26 +0600

    escaped characters in the question data unescaped

--- quizzler-app/quiz_brain.py
+++ quizzler-app/quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain:
 
     def __init__(self, question_list):
@@ -8,7 +10,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.question} (True/False): ")
+        current_question_unescaped = html.unescape(current_question.question)
+        user_answer = input(f"Q.{self.question_number}: {current_question_unescaped} (True/False): ")
         self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
