0fc8ea1824166818b050b35f011d848e0c0ec147
shams-ivon <shams919ivon@gmail.com>
2024-01-22 00:24:29 +0600

    title, link and, vote count scraped

--- web-scraping-basics/main.py
+++ web-scraping-basics/main.py
@@ -7,8 +7,22 @@ yc_web_page = responses.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 
 news_articles = soup.find_all(name="span", class_="titleline")
-news_upvotes = soup.find_all(name="span", class_="subline")
+news_votes = soup.find_all(name="span", class_="score")
+
+news_article_title_list = []
+news_article_link_list = []
+news_article_vote_list = []
 
 for article in news_articles:
-    print(article.getText())
-    print(article.a.get("href"))
+    news_article_title_list.append(article.getText())
+    news_article_link_list.append(article.a.get("href"))
+
+for article in news_votes:
+    vote = article.text.split()[0]
+    news_article_vote_list.append(int(vote))
+
+for index in range(len(news_article_title_list)):
+    print(news_article_title_list[index])
+    print(news_article_link_list[index])
+    print(news_article_vote_list[index])
+    print("")
